7c286e704d0d16eed1f606b901f017448bb684d9
Travis Clarke <spardact@gmail.com>
2019-08-14 18:07:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function sayHiToGrandma(string) {
-  console.log("hello!")
-  return("i can't hear you!") 
-}
+function whisper(string) {
+  return string.toLowerCase()
